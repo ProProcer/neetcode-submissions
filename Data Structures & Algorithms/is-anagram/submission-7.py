5fb7fac6d1f3d6e707c84bcd6ae57047ae0885b7
Matthew Alexander Paudianto <39817115+ProProcer@users.noreply.github.com>
2026-09-04 22:35:33 +0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        set_s = set(s)
+        if set_s != set(t):
+            return False
+        
+        count = defaultdict(int)
+        for i, j in zip(s, t):
+            count[i] += 1
+            count[j] -= 1
+        
+        for i in count.values():
+            if i != 0:
+                return False
+
+        return True
