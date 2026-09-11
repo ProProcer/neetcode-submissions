716ff777872f402a4659ec4aeb51baea41b97e39
Matthew Alexander Paudianto <39817115+ProProcer@users.noreply.github.com>
2026-09-11 08:26:39 +0700

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.py
@@ -0,0 +1,38 @@
+class TriesNode:
+    def __init__(self):
+        self.children = {}
+        self.exist = False
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TriesNode()
+        
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TriesNode()
+            curr = curr.children[c]
+        curr.exist = True
+    def search(self, word: str) -> bool:
+        
+        def is_exist(idx, root):
+            curr = root
+            for i in range(idx, len(word)):
+        
+                c = word[i]
+                if c == '.':
+                    for child in curr.children.values():
+                        exist = is_exist(i + 1, child)
+                        if exist:
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+
+            return curr.exist
+        return is_exist(0, self.root)
